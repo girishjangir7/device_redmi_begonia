4e23953997a4be2f81cb14c88f21ad969f29c12f
Peter Cai <peter@typeblog.net>
2019-11-08 19:41:55 +0800

    begonia: sepolicy: address proc_ged denials
    * They are just too annoying in logs (our ROM still does not work in
    enforcing mode though)
    * Thanks to phhusson's GSI device tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -128,6 +128,9 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
+# Sepolicy
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+
 # Treble
 TARGET_COPY_OUT_VENDOR := vendor
 BOARD_VNDK_VERSION := current
